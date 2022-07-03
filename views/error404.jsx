ee6c0d37285b3675f808cb0671d508b8c07a9a9b
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-07-03 12:49:33 -0400

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,6 +8,11 @@ function error404 () {
         <main>
             <h1>404: PAGE NOT FOUND</h1>
             <p>WE AINT FOUND SHIT!</p>
+            <div>
+              <img src="\images\ice-cream.jpg" alt="broken ice cream" />
+              <div>Photo by <a href="https://unsplash.com/@rojekilian?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sarah Kilian</a> on <a href="https://unsplash.com/s/photos/404?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
+            </div>
         </main>
     </Def>
   )
--- views/home.jsx
+++ views/home.jsx
@@ -7,6 +7,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+            <div>
+                    <img src="\images\kebab.jpg" alt="kabob pic" />
+                <div>
+                    photo by <a href="https://unsplash.com/@victoriakosmo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Victoria Shes</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+            </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
