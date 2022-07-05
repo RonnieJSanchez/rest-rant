f8636ef6be4a0f091fdf9811c3cf677f0391308b
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-07-05 14:24:17 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,13 +1,14 @@
-const router = require ('express').Router();
-const places = require('../models/places.js')
+const router = require("express").Router();
+const places = require("../models/places.js")
 
-router.get('/', (req, res) => {
-  res.render('places/index', {places})
-})
-
-router.get('/new', (req, res) => {
-  res.render('places/new');
+router.get("/", (req, res) => {
+  res.render("places/index", { places });
 });
+
+router.get("/new", (req, res) => {
+  res.render('places/new')
+});
+
 
 router.post('/', (req, res) => {
   console.log(req.body)
@@ -24,28 +25,28 @@ router.post('/', (req, res) => {
   }
   places.push(req.body)
   res.redirect('/places')
-})
-
-router.get('/', (req,res) => {
-  let places = [
-{
-  name: 'H-Thai-ML',
-  city: 'Seattle',
-  state: 'WA',
-  cuisines: 'Thai, Pan-Asian',
-  pic: '/images/thai-food.jpg'
-}, 
-{
-  name: 'Coding Cat Cafe',
-  city: 'Phoenix',
-  state: 'AZ',
-  cuisines: 'Coffee, Bakery',
-  pic: '/images/coffee.jpg'
-},
-];
-
-  res.render('places/index', {places});
-
 });
+
+//router.get('/', (req,res) => {
+//  let places = [
+//{
+//  name: 'H-Thai-ML',
+//  city: 'Seattle',
+//  state: 'WA',
+//  cuisines: 'Thai, Pan-Asian',
+//  pic: '/images/thai-food.jpg'
+//}, 
+//{
+//  name: 'Coding Cat Cafe',
+//  city: 'Phoenix',
+//  state: 'AZ',
+//  cuisines: 'Coffee, Bakery',
+//  pic: '/images/coffee.jpg'
+//},
+//];
+//
+//  res.render('places/index', {places});
+//
+//});
 
 module.exports = router 
--- views/home.jsx
+++ views/home.jsx
@@ -11,7 +11,7 @@ function home () {
                     <button className="btn-primary">Places Page</button>
                 </a>
                 <div>
-                    <img src="\images\kebab.jpg" alt="kabob pic" />
+                    <img src="\images\kebab.jpg" alt="kebab pic" />
                     <div>
                         photo by <a href="https://unsplash.com/@victoriakosmo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Victoria Shes</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
