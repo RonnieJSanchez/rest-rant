54e727b948b227a5569738cf1b6926961b5c1d84
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-07-14 15:44:27 -0400

    implemented changes to the show.jsx folder

--- models/places.js
+++ models/places.js
@@ -4,9 +4,13 @@ const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
   pic: String,
   cuisines: { type: String, required: true },
-  city: { type: String, default: 'Timbuctoo' },
-  state: { type: String, default: 'CA' },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
   founded: Number
 })
+
+placeSchema.methods.showEstablished = function() {
+  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
 
 module.exports = mongoose.model('Place', placeSchema)
